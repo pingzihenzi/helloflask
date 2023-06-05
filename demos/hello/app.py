875c261c2880d4426c3256d98f0e98f05cccf57e
asce <190970161@qq.com>
2023-06-05 14:57:51 +0800

    odify

--- demos/hello/app.py
+++ demos/hello/app.py
@@ -14,7 +14,7 @@ app = Flask(__name__)
 # the minimal Flask application
 @app.route('/')
 def index():
-    return '<h1>Hello, World!</h1>'
+    return '<h1>Hello, World!!</h1>'
 
 
 # bind multiple URL for one view function
